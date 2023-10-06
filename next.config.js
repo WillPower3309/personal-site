c8f43c2284170ab1097d0f67512f40f741bb1205
William McKinnon <contact@willmckinnon.com>
2023-10-06 01:58:10 -0400

    added contentlayer md logic

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const { withContentlayer } = require('next-contentlayer')
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
     output: 'export',
 }
 
-module.exports = nextConfig
+module.exports = withContentlayer(nextConfig)
---
+++ src/posts/test.md
@@ -0,0 +1,9 @@
+---
+title: Test title
+date: 2021-12-24
+---
+
+1. test list
+2. test list
+
+test content
